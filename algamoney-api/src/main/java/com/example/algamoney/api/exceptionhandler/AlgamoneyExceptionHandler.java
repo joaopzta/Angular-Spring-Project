e5fd6780cde1ab34b4476bd0ce39e44e66172226
João Pedro Zanqueta <joaopzta@gmail.com>
2020-09-23 11:18:06 -0300

    5.5 Validando lançamentos com Bean Validation

--- algamoney-api/src/main/java/com/example/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -49,6 +50,15 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 	public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex, WebRequest request) {
 		String mensagemUsuario = messageSource.getMessage("recurso.nao-encontrado", null, LocaleContextHolder.getLocale());
 		String mensagemDesenvolvedor = ex.toString();
+		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+		
+		return super.handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+	}
+	
+	@ExceptionHandler({ DataIntegrityViolationException.class })
+	public ResponseEntity<Object> handleDataItegerityValitationException(DataIntegrityViolationException ex, WebRequest request) {
+		String mensagemUsuario = messageSource.getMessage("recurso.operacao-nao-permitida", null, LocaleContextHolder.getLocale());
+		String mensagemDesenvolvedor = ex.getMostSpecificCause().toString();
 		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
 		
 		return super.handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
--- algamoney-api/src/main/java/com/example/algamoney/api/model/Lancamento.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/model/Lancamento.java
@@ -13,6 +13,9 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 @Table(name = "lancamento")
@@ -21,25 +24,35 @@ public class Lancamento {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
+	
+	@NotNull
 	private String descricao;
 	
 	@Column(name = "data_vencimento")
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	@NotNull
 	private LocalDate dataVencimento;
 	
 	@Column(name = "data_pagamento")
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataPagamento;
+	
+	@NotNull
 	private BigDecimal valor;
 	private String observacao;
 	
 	@Enumerated(EnumType.STRING)
+	@NotNull
 	private TipoLancamento tipo;
 	
 	@ManyToOne
 	@JoinColumn(name = "codigo_categoria")
+	@NotNull
 	private Categoria categoria;
 	
 	@ManyToOne
 	@JoinColumn(name = "codigo_pessoa")
+	@NotNull
 	private Pessoa pessoa;
 
 	public Long getCodigo() {
